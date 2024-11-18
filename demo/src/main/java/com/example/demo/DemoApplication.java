d088d81b5a3c34789024c490c3fb86fa5c64d858
SROSATI <srosati1980@gmail.com>
2024-11-18 11:56:01 +0100

    test 11.55

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//test555
+//test ore 11.55
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
